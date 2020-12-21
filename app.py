8939ffcd6340e361b7f8dcafd18a261117db92f4
LhetMagnan <37093662+LhetMagnan@users.noreply.github.com>
2020-12-21 19:46:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import flask
 import pickle
 import re
-from sklearn.model_selection import train_test_split
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from keras.models import load_model 
